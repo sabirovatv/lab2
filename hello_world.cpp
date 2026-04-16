57c8b4ef75713fad6e719281ec03972b2c5dd53c
sabirovatv <sabirovatamara77@gmail.com>
2026-04-16 10:19:44 +0300

    docs: add inline comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,8 +4,10 @@
 int main()
 {
   std::string name;
+  // Запрашиваем имя пользователя через стандартный поток ввода
   std::cout << "Enter your name: ";
   std::cin >> name;
+  // Выводим приветствие в стандартный поток вывода
   std::cout << "Hello world from @" << name << std::endl;
   return 0;
 }
